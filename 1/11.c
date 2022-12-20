bca27ac764a6240244bd7b803337224fd3bcbd48
Cian Bagshaw <cian@cianb.xyz>
2022-12-20 23:16:57 +0000

    1-11

---
+++ 1/11.c
@@ -0,0 +1,10 @@
+/*
+	Exercise 1-11. How would you test the word count program? What kinds of input
+	are most likely to uncover bugs if there are any?
+	===
+
+	One could test the program by giving it different character inputs of decent
+	length, to see if the program counts everything. Hyphenated words would cause
+	an issue as they would be counted as one, since they are not separated by
+	whitespace.
+*/
