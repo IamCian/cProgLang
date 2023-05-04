56dcfa9375782c95b7d92aea3215e5541507b00d
Cian Bagshaw <cian@cianb.xyz>
2023-05-04 22:35:06 +0100

    1-13a
    Made vertical histogram

--- 1/13a.c
+++ 1/13a.c
@@ -10,25 +10,31 @@
 #define BUF 50	/* max word length */
 
 int main () {
-	int c, l, i, lngst;	/* character, length, index, longest */
+	int c, l, i, m, lngst;	/* character, length, index, most, longest */
 	int count[BUF];		/* length count */
 	
 	/* initialise to zero */
 	for (i=0; i<BUF; i++) count[i]=0;
-	lngst=l=0;
+	lngst=m=l=0;
 
 	while ((c=getchar())!=EOF) {
 		if (c==' ' || c=='\t' || c=='\n') {	/* space */
 			if (l) {	/* if word just ended */
 				if (l>lngst) lngst=l;	/* update longest */
-				count[l-1]++;	/* add length to count */
-				l=0;			/* reset length count */
+				count[l-1]++;			/* add length to count */
+				if (count[l-1]>m) m=count[l-1];	/* update most */
+				l=0;					/* reset length count */
 			}
 		} else l++;	/* count word length */
 	}
-	while (lngst--) {	/* for each count */
-		printf("%2.d|", lngst+1);			/* length */
-		while (count[lngst]--) putchar('-');/* count (bar) */
-		putchar('\n');						/* newline */
-	}	
+	while (m--) {	/* for each line */
+		for (i=0; i<=lngst-1; i++) {	/* for each count */
+			if (count[i]>=m+1)	printf(" | ");
+			else				printf("   ");
+		} putchar('\n');
+	}
+	for (i=1; i<=lngst; i++)	/* labels */
+		printf("%2.d ", i);
+	putchar('\n');
+
 }
